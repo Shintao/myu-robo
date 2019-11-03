436c974b52f73825fd7bd4872657375113e1ab61
Shintao <57308066+Shintao@users.noreply.github.com>
2019-11-04 07:57:44 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+namespace basic {
+
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+
+}
